23aa9900cbcba17800ba8eed968242d6b0622a8a
Colum O' Callaghan <camcallaghan@gmail.com>
2016-01-08 10:33:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notif::Application.config.secret_key_base = 'd9463809ee6c814b3bade65c760fbda2d8b0947859c47aa526dd0fb5815a93c000b5db6af265b7e09172bec45e55489328f791f3d7b698a9323776a38e49e6cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Notif::Application.config.session_store :cookie_store, key: '_notif_session'
