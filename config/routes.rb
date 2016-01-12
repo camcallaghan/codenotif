d58fcbdfd1fc43c0ccbaba259148a8e1e830d9e4
Colum O' Callaghan <camcallaghan@gmail.com>
2016-01-12 23:50:07 +0000

    Editing my routes file for new controller and paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Notif::Application.routes.draw do
-  root 'quotes#index'
+  root 'reviews#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
